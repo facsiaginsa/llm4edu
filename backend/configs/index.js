d29a6e70eb8a774478ca6cf23c66353778ba1dfe
Ivan Muhammad Siegfried <ivanmuhsiegfried@gmail.com>
2024-10-28 00:14:24 +0700

    Done implementing Add-ins and Backend for brainstorm-title feature (With OpenAI API)

--- backend/configs/index.js
+++ backend/configs/index.js
@@ -3,5 +3,6 @@ require("dotenv").config()
 module.exports = {
     PORT: process.env.PORT,
     IP_BIND: process.env.IP_BIND,
-    MONGO_CONN_STRING: process.env.MONGO_CONN_STRING
+    MONGO_CONN_STRING: process.env.MONGO_CONN_STRING,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY
 }
