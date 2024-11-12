283189068df5e6a1cc0e6b5605d586bcb4aaa4ab
Bagus F <facsi.aginsa@gmail.com>
2024-11-12 21:08:54 +0700

    improving brainstorming response

--- backend/modules/brainstorm/model.js
+++ backend/modules/brainstorm/model.js
@@ -46,27 +46,6 @@ let sendBrainstorm = async (conversation) => {
             
             `);
         */
-        const prompt = PromptTemplate.fromTemplate(`
-            You are a helpful researcher.
-
-            Your task is to generate 3 to 5 insightful research ideas related to the given research area. 
-            Each research idea should include a title that is one sentence long, and an abstract that is 3-5 sentences long. 
-            You may use some of the provided research as reference, 
-            but please also leverage your expertise as a researcher to generate these ideas.
-
-            research:
-            {context}
-            
-            research area: {content}
-
-            idea:
-
-            please follow this html template for your ideas:
-            <strong><p>[The title of research]</p></strong>
-            <strong><p>Abstract:</strong> [The abstract of the research]</p>
-            <br/>
-            `
-        )
 
         // Collection Vector
         const vectorStore = await collectionWithVector(collection);
@@ -79,12 +58,66 @@ let sendBrainstorm = async (conversation) => {
             
         let filteredOutput = basicOutput.filter(document => document[1] > 0.6)
 
-        // Chain
-        const llm = converse(0.8, 0.9)
-        const chain = prompt.pipe(llm);
-        const response = await chain.invoke({ content: conversation, context: basicOutput});
-        typeof("di model: ", typeof(response))
-        //return await converse.stream(conversation)
+        let llm = converse(0.8, 0.9)
+        let response = ""
+        let prompt = ""
+        console.log(filteredOutput)
+        if (filteredOutput.length > 0) {
+            let context = ""
+            filteredOutput.forEach((record) => {
+                context = context + "\n\n" + record[0].pageContent
+            })
+
+            prompt = PromptTemplate.fromTemplate(`
+                You are a helpful researcher.
+    
+                Your task is to generate 3 to 5 insightful research ideas related to the given research area. 
+                Each research idea should include a title that is one sentence long, an abstract that is 3-5 sentences long,
+                and add 3-4 keyword related to the research idea seperated by comma.  
+                You may use some of the provided research as reference, 
+                but please also leverage your expertise as a researcher to generate these ideas.
+    
+                research: {context}
+                
+                research area: {content}
+    
+                idea:
+    
+                please follow this html template for your ideas:
+                <strong><p>[The title of research]</p></strong>
+                <strong><p>Abstract:</strong> [The abstract of the research]</p>
+                <p><strong>Keywords:</strong> [The keywords of the idea]</p>
+                <br/>
+                `
+            )
+
+            const chain = prompt.pipe(llm);
+            response = await chain.invoke({ content: conversation, context: context});
+        } else {
+            prompt = PromptTemplate.fromTemplate(`
+                You are a helpful researcher.
+    
+                Your task is to generate 3 to 5 insightful research ideas related to the given research area. 
+                Each research idea should include a title that is one sentence long, an abstract that is 3-5 sentences long,
+                and add 3-4 keyword related to the research idea seperated by comma. 
+                Please leverage your expertise as a researcher to generate these ideas.
+                
+                research area: {content}
+    
+                idea:
+    
+                Please follow this html template for your ideas:
+                <p><strong>[The title of research]</strong></p>
+                <p><strong>Abstract:</strong> [The abstract of the research]</p>
+                <p><strong>Keywords:</strong> [The keywords of the idea]</p>
+                <br/>
+                `
+            )
+
+            const chain = prompt.pipe(llm);
+            response = await chain.invoke({ content: conversation});
+        }
+        
     return response
 }
 
